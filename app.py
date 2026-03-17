5470c41787a84bdecde1407e680c5e86ccf2d8c5
Daniel Lima <daniellimajse@gmail.com>
2026-03-16 21:33:30 -0300

    teste

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
-print('Bom dia!')
-print('meu nome é Daniel')
+este é o arquivo da da branch main
+
+
+
+
+
+#git add .
+
+#git commit -m "branch de daniel"
+
+#git push -u origin main - manda pra nuvem (github)
+
+#git pull - puxa pro local
+
+#git branch - criar uma nova branch
+
+#git push --set-upstream origin daniel
